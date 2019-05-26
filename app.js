2ef4c279e5de1302dc9fb9145f247e2dee307db5
Lakshyajit <lakshyajit@localhost.localdomain>
2019-05-26 19:37:04 +0530

    Dynamic Realtions - 1

--- app.js
+++ app.js
@@ -14,6 +14,35 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = eventIds => {
+  return Event.find({_id: {$in: eventIds}})
+  .then(events => {
+    return events.map(event => {
+      return {
+        ...event._doc, 
+        _id: event.id, 
+        creator: user.bind(this, event.creator)};
+    });
+  })
+  .catch(err => {
+    throw err;
+  })
+}
+
+const user = userId => {
+  return User.findById(userId)
+    .then(user => {
+      return { 
+        ...user._doc, 
+        _id: user.id, 
+        createdEvents: events.bind(this, user._doc.createdEvents) 
+      };
+    })
+    .catch(err => {
+      throw err;
+    });
+}
+
 app.use(
   '/graphql',
   graphqlHttp({
@@ -24,12 +53,14 @@ app.use(
           description: String!
           price: Float!
           date: String!
+          creator: User!
         }
 
         type User{
            _id: ID!
            email: String!
            password: String 
+           createdEvents: [Event!]
         }
 
         input EventInput {
@@ -58,9 +89,14 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return Event.find().then(events => {
+        return Event.find()
+        .then(events => {
           return events.map(event => {
-            return {...event._doc, _id: event.id };
+            return {
+              ...event._doc, 
+              _id: event.id,
+              creator: user.bind(this, event._doc.creator)
+            };
           });
         }).catch(err => {
           throw err;
@@ -87,7 +123,11 @@ app.use(
         return event
         .save()
         .then(result => {
-          createdEvent = {...result._doc, id: result._doc._id.toString() };
+          createdEvent = {
+            ...result._doc, 
+            id: result._doc._id.toString(), 
+            creator: user.bind(this, result._doc.creator) 
+          };
           return User.findById('5ce29e648db94212d2bbafa6')
           //console.log(result);
           
